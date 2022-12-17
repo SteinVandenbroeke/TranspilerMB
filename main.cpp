304856f37b6c1aec7d969b6cff12e5e95539b0f6
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2022-12-17 18:30:56 +0100

    Arguments TODO duidelijker en beter cli interface

--- main.cpp
+++ main.cpp
@@ -17,68 +17,21 @@ void cstConstructionTest() {
     }
 }
 
-int main() {
-    WebServer ws = WebServer();
-    ws.start();
-/*
-    Tokenizer t = Tokenizer();
-    std::vector<Token*> tokens = t.convert("inputs/STaalCode.txt");
-    CST cst = CST(tokens,"LR1ParserGenerator/test_grammar_0.json_PARSETABLE.json");
-   // std::cout << cst.generateDOT() << std::endl;
-    AstProgram* program = cst.toAst();
-
-    if(program->checkTypes()){
-        std::cout << program->getJsCode() << std::endl;
+int main(int argc, char *argv[]) {
+    if(argc == 1 || argv[1] == "-webInterface"){
+        WebServer ws = WebServer();
+        ws.start();
     }
-   // std::cout << program->generateDOT() << std::endl << std::endl;
+    else if(argc == 2){
+        Tokenizer t = Tokenizer();
+        std::vector<Token*> tokens = t.convert(argv[1]);
+        CST cst = CST(tokens,"LR1ParserGenerator/test_grammar_0.json_PARSETABLE.json");
+        // std::cout << cst.generateDOT() << std::endl;
+        AstProgram* program = cst.toAst();
 
-    //Tokenizer testing
-
-    //ENFA testing
-    /*
-    ENFA e = ENFA("inputs/input1.json");
-    e.print();
-    for(auto s : e.closure(e.getStartState())){
-        std::cout << s->getName() << std::endl;
+        if(program->checkTypes()){
+            std::cout << program->getJsCode() << std::endl;
+        }
     }
-    std::cout << std::endl << e.accepts("0.0") << std::endl;
-    */
-
-/*
-    Token* tokenVarA = new Token("[VARNAMETOKEN]", "a");
-    AstVar* varA = new AstVar(tokenVarA);
-
-    Token* tokenVarI = new Token("[VARNAMETOKEN]", "i");
-    AstVar* varI = new AstVar(tokenVarI);
-
-    Token* tokenDeclarationA = new Token("int");
-    Token* tokenDeclarationI = new Token("int");
-
-    Token* number = new Token("[NUMBERTOKEN]", "10");
-    AstValue* value = new AstValue(number);
-
-//    std::vector<Token *> tokenList{new Token("int"),new Token("[VARNAMETOKEN]","i"),new Token("="),new Token("[NUMBERTOKEN]", "10"),new Token(";"),new Token("while"),new Token("("),new Token("[VARNAMETOKEN]","i"), new Token("<"), new Token("[NUMBERTOKEN]","11"),new Token(")"),new Token("{"), new Token("int"),new Token("[VARNAMETOKEN]","a"),new Token("="),new Token("[NUMBERTOKEN]", "10"),new Token(";"),new Token("}"), new Token("$")};
-//    CST cst = CST(tokenList,"LR1ParserGenerator/test_grammar_0.json_PARSETABLE.json");
-//    cst.generateDOT();
-
-    AstDeclartion* declartionI = new AstDeclartion(tokenDeclarationI, varI, value);
-    AstDeclartion* declartionA = new AstDeclartion(tokenDeclarationA, varA, value);
-
-    AstIntalisation* intalisation = new AstIntalisation(varI, varA);
-
-
-    AstProgram program = AstProgram();
-    program.addLine(declartionI);
-    program.addLine(declartionA);
-    //program.addLine(declartionA);
-    //program.addLine(declartion);
-    //program.addLine(declartion);
-    program.addLine(intalisation);
-    program.checkTypes();
-    std::cout << program.getJsCode();
-    std::cout << program.generateDOT();
-    */
-   // cstConstructionTest();
-
     return 0;
 }
